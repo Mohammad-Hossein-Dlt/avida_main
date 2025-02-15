6bd75eac36cf9196dfc1b522fabedec3f21106d8
Mohammad_Hosein.Dlt <hosein27d@gmail.com>
2025-02-16 01:25:49 +0330

    add auto instal

--- install.sh
+++ install.sh
@@ -17,9 +17,8 @@ echo "Cloning repository..."
 
 # Replace 'your-token' with your actual GitHub token
 REPO_URL="Mohammad-Hossein-Dlt/avida_main"
-TOKEN="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"
 
-git clone https://$TOKEN@github.com/$REPO_URL /home/fastapi-project
+git clone https://github.com/$REPO_URL /home/fastapi-project
 
 # Navigate to project directory
 cd /home/fastapi-project || exit
@@ -36,3 +35,4 @@ docker run -d -p 8000:8000 --name fastapi-container fastapi-app
 echo "FastAPI is now running on http://localhost:8000"
 
 
+bash <(curl -Ls -u https://raw.githubusercontent.com/Mohammad-Hossein-Dlt/avida_main/master/scripts/install.sh)
