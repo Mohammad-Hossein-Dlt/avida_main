f3bfe3880f573e2c94bb0ad603e8c5b5e699d19c
Mohammad_Hosein.Dlt <79458791+Mohammad-Hossein-Dlt@users.noreply.github.com>
2025-09-13 18:28:36 +0330

    Update config.py

--- config/config.py
+++ config/config.py
@@ -2,25 +2,25 @@
 # ------------------------------ Database ------------------------------
 
 DB_USER = "root"
-DB_PASSWORD = "tP5oawUFTNmLZ7P99C5VbhlP"
-DB_HOST = "logan.liara.cloud"
-DB_PORT = "30519"
-DB_NAME = "Avida"
+DB_PASSWORD = "rootpassword"
+DB_HOST = "mongo-db"
+DB_PORT = "27017"
+DB_NAME = "db"
 
 # ------------------------------ Storage ------------------------------
 
-STORAGE_ENDPOINT = "https://storage.iran.liara.space"
-STORAGE_ACCESS_KEY = 'imp0khi7nmor93qi'
-STORAGE_SECRET_KEY = '818e90b8-c454-4f92-be35-d197e895ea26'
+STORAGE_ENDPOINT = ""
+STORAGE_ACCESS_KEY = ""
+STORAGE_SECRET_KEY = ""
 
 # ------------------------------ Broker ------------------------------
 
-BROKER_URL = "amqp://root:IHngMCMYKTb5BFABwd1H1EEL@logan.liara.cloud:33499"
+BROKER_URL = ""
 
 # ------------------------------ GPT ------------------------------
 
-GPT_TOKEN = "aa-1XBNjgKUvEWffunOFEEuPbFopQHuBLTRwjRq8Od2g7A1bOkW"
+GPT_TOKEN = ""
 
 # ------------------------------ Jwt Secret  ------------------------------
 
-JWT_SECRET = "b5670866-a347-4020-9718-b870b324c892"
+JWT_SECRET = ""
